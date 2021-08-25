1dbb153b640edbf07f2070a4bdd3a10e07456416
İrem Pınar <irembusekeskin@gmail.com>
2021-08-26 00:20:54 +0300

    layout denemesi

---
+++ WebApplication1/Controllers/TatlılarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TatlılarController : Controller
+    {
+        // GET: Tatlılar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
